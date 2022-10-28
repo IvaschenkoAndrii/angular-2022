40e544230920a19608837b1f41d74a47ab7fc139
IvaschenkoAndrii <ivaschenko84@gmail.com>
2022-10-28 16:03:15 +0300

    lesson1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
-import { UsersComponent } from './components/users/users.component';
-import { PostsComponent } from './components/posts/posts.component';
-import { CommentsComponent } from './components/comments/comments.component';
-import { UserComponent } from './components/user/user.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppComponent} from './app.component';
+import {UsersComponent} from './components/users/users.component';
+import {PostsComponent} from './components/posts/posts.component';
+import {CommentsComponent} from './components/comments/comments.component';
 import {HttpClientModule} from "@angular/common/http";
-import { PostComponent } from './components/post/post.component';
-import { CommentComponent } from './components/comment/comment.component';
+import {CommentComponent} from './components/comment/comment.component';
+import {RouterModule, Routes} from "@angular/router";
+
+import {PostComponent} from './components/post/post.component';
+import {UserComponent} from './components/user/user.component';
+import {MainLayoutComponent} from './components/layouts/main-layout/main-layout.component';
+import {HeaaderComponent} from './components/header/heaader/heaader.component';
+import * as path from "path";
+import { UserDetailsComponent } from './components/user-details/user-details.component';
+
+let routes: Routes = [
+  {
+    path: '', component: MainLayoutComponent, children: [
+      {path:'',redirectTo:'users',pathMatch:"full"},
+      {
+        path: 'users', component: UsersComponent,children:[
+          {path:':id',component:UserDetailsComponent}
+        ]
+      },
+      {path: 'posts', component: PostsComponent},
+      {path: 'comments', component: CommentsComponent}
+    ]
+  },
+
+];
 
 @NgModule({
   declarations: [
@@ -18,13 +39,18 @@ import { CommentComponent } from './components/comment/comment.component';
     CommentsComponent,
     UserComponent,
     PostComponent,
-    CommentComponent
+    CommentComponent,
+    MainLayoutComponent,
+    HeaaderComponent,
+    UserDetailsComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
