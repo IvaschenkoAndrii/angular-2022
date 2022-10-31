fe8acda4c5c71bf352faac7464600714a7a16a30
IvaschenkoAndrii <ivaschenko84@gmail.com>
2022-10-31 17:54:04 +0200

    lesson3

--- src/app/modules/comment/components/comment-detail/comment-detail.component.ts
+++ src/app/modules/comment/components/comment-detail/comment-detail.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {IComment} from "../../interfaces";
+import {CommentService} from "../../services";
 
 @Component({
   selector: 'app-comment-detail',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./comment-detail.component.css']
 })
 export class CommentDetailComponent implements OnInit {
+  comment: IComment;
 
-  constructor() { }
+  constructor(private router: Router, private activatedRoute: ActivatedRoute, private commentService: CommentService) {
+    this.activatedRoute.params.subscribe(({id}) => {
+      this.comment = this.router.getCurrentNavigation()?.extras.state?.['comment'];
+
+      // commentService.getById(id).subscribe(value => this.comment = value);
+
+
+    })
+  }
 
   ngOnInit(): void {
+
   }
 
 }
